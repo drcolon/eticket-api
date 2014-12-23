485386f2390168571d27c4a90aef2e56f580a8a9
Daniel Colón Montalvo <dcolonmontalvo@gmail.com>
2014-12-22 22:55:19 -0500

    Ha sido configurado el ambiente básico de mocha

---
+++ test/testMocha.js
@@ -0,0 +1,13 @@
+describe('Prueba Mocha',function(){
+  it('Debería funcionar el test', function(done){
+
+    done();
+  });
+
+  it('Debería volver a funcionar el test', function(done){
+
+    done();
+  });
+
+
+});
